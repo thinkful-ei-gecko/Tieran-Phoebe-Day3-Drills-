26464c6de6c7b0febe7cd8e744f58df26831e5d4
Phoebe Law <phoebebasilio@gmail.com>
2019-08-14 09:51:03 -0700

    Finished Object Updater Drill

--- object.js
+++ object.js
@@ -10,3 +10,18 @@ function createMyObject(){
 }
 
 console.log(createMyObject());
+
+const newObj = {
+    name: 'Phoebe',
+    lastName: 'Law',
+}
+
+function updateObject(obj) {
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    return obj;
+}
+
+console.log(updateObject(newObj));
