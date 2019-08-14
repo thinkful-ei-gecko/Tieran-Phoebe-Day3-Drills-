bd59e3b2f43bdc39d627d70edbf2acb76775eee3
Phoebe Law <phoebebasilio@gmail.com>
2019-08-14 11:10:46 -0700

    Deleting Keys finished

--- object.js
+++ object.js
@@ -43,3 +43,20 @@ function personMaker() {
   }
    console.log(personMaker());
   
+// deleting keys
+
+const sampleObj = {
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  };
+  
+function keyDeleter(obj) {
+    // your code here
+    delete obj.foo;
+    delete obj.bar;
+    return obj;
+}
+
+console.log(keyDeleter(sampleObj));  
