c08fde654ba4cc89f7fa1d549c11f24080e3b70c
Tieran Dysart <tierandysart@gmail.com>
2019-08-14 10:10:50 -0600

    first problem done

---
+++ object.js
@@ -0,0 +1,12 @@
+function createMyObject(){
+    return  obj = {
+        foo:'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello (){
+        return  'hello';
+        }
+}
+}
+
+console.log(createMyObject());
